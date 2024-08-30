4b8ee2f21a65d327d1fd4e358a206b85c2356590
Yosef Fourentino <yoseffourentino30@gmail.com>
2024-08-30 21:53:40 +0700

    .

--- news_portal_project/src/Api.js
+++ news_portal_project/src/Api.js
@@ -6,10 +6,10 @@ const api_Key = "30788c48d65b4ed7803ad66cf60698fb"
 
 export const getNewsList = async() => {
     const news = await axios.get(`${base_url}/top-headlines?country=id&apiKey=${api_Key}`)
-    console.log(news)
+    return news.data.articles; 
 }
 
-export const searchNews = async() => {
-    const search = await axios.get()
-    return
-}
+// export const searchNews = async() => {
+//     const search = await axios.get()
+//     return
+// }
--- news_portal_project/src/components/News.jsx
+++ news_portal_project/src/components/News.jsx
@@ -1,16 +1,35 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getNewsList } from "../Api"
 
 
 export default function News(){
+    const [news, setNews] = useState([]);
+
     useEffect(() => {
-        getNewsList()
+        getNewsList().then((result) => {
+            setNews(result)
+        })
     }, [])
+
+    console.log(news)
 
     const search = (q) => {
         console.log({q})
     }
     
+    const NewsList = () => {
+        return news.map((news, i) => {
+            return (
+                <div className="bg-[#ffffff]" key={i}>
+                        <h2 className="news-title">{news.title}</h2>
+                        <img className="news-image" src="" alt="" />
+                        <p className="news-author">{news.author}</p>
+                        <p className="news-date">{news.publishedAt}</p>
+                    </div>
+            )
+        })
+    }
+
     return(
         <div className="bg-[#E2DAD6] flex justify-center">
             <div className="bg-[#F5EDED] flex flex-col justify-center items-center w-[1366px] p-[30px]"> 
@@ -20,13 +39,8 @@ export default function News(){
                     className="w-[600px] h-[35px] text-center mb-[20px]" 
                     onChange={({target}) => search(target.value)}
                 />
-                <div className="news-container grid grid-cols-3 gap-5 ">
-                    <div className="bg-[#ffffff]">
-                        <h2 className="news-title">Stock market today: Dow hits record high while tech shares stumble ahead of Nvidia earnings</h2>
-                        <img className="news-image" src="" alt="" />
-                        <p className="news-desc">The Dow hit a record high on Monday. Tech shares edged lower ahead of the hotly anticipated Nvidia earning report scheduled for Wednesday</p>
-                        <p className="news-date">2024-08-26T20:10:10Z</p>
-                    </div>
+                <div className="news-container grid grid-cols-3 gap-5 ">                   
+                    <NewsList />
                 </div>   
             </div>
         </div>

b33cc2bbceb78518b63b8066e823897fa5316890
Yosef Fourentino <yoseffourentino30@gmail.com>
2024-11-19 13:22:15 +0700

    backend

---
+++ backend/controllers/articleController.js
@@ -0,0 +1,88 @@
+const Article = require('../models/articleModel')
+const mongoose = require('mongoose')
+
+//get all Articles
+
+const getArticles = async (req,res) => {
+    const articles = await Article.find({}).sort({createdAt: 1})
+
+    res.status(200).json(articles)
+}
+
+//get a single Article
+
+const getArticle = async (req,res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: "Not a Valid ID"})
+    }
+
+    const article = await Article.findById(id)
+
+    if(!article){
+        res.status(404).json({error: "No such Article"})
+    }
+    res.status(200).json(article)
+}
+
+//create a new Article
+
+const createArticle =  async (req,res) => {
+    const { title, category, author, image, desc, content} = req.body
+
+    //add doc to db
+    try{
+        const article = await Article.create({ title, category, author, image, desc, content})
+        res.status(200).json(article)
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+//update a new Article
+
+const updateArticle = async (req,res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: "Not a Valid ID"})
+    }
+
+    const article = await Article.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if(!article){
+        res.status(404).json({error: "NO! such Article being UPDATED"})
+    }
+
+    res.status(200).json(article)
+}
+
+//delete a single article
+
+const deleteArticle = async (req,res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: "Not a Valid ID"})
+    }
+
+    const article = await Article.findOneAndDelete({_id: id})
+
+    if(!article){
+        res.status(404).json({error: "NO! such Article being DELETED"})
+    }
+
+    res.status(200).json(article)
+
+}
+
+module.exports = {
+    getArticles,
+    getArticle,
+    createArticle,
+    deleteArticle,
+    updateArticle
+}
--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -1,38 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const Article = require('../models/articleModel')
+const { getArticles, getArticle, createArticle, deleteArticle, updateArticle } = require('../controllers/articleController')
 
 require('dotenv').config()
 
 //GET all articles
-router.get('/', (req,res) => {
-    res.json({mssg:'GET all articles'})
-})
+router.get('/', getArticles)
 
 //GET a single article
-router.get('/:id', (req,res) => {
-    res.json({mssg:'GET a single article'})
-})
+router.get('/:id', getArticle )
 
 //POST a new article
-router.post('/', (req,res) => {
-    const { title, category, author, image, desc, content} = req.body
-    try{
-        const article = Article.create({ title, category, author, image, desc, content})
-        res.status(200).json(article)
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
-})
+router.post('/', createArticle )
 
 //DELETE a single article
-router.delete('/:id', (req,res) => {
-    res.json({mssg:'DELETE a single article'})
-})
+router.delete('/:id', deleteArticle )
 
 //UPDATE an article
-router.patch('/:id', (req,res) => {
-    res.json({mssg:'UPDATE an article'})
-})
+router.patch('/:id', updateArticle )
 
 module.exports = router

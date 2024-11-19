78f97461d0baa441985afbf79a442410c0330d66
Yosef Fourentino <yoseffourentino30@gmail.com>
2024-11-19 16:56:09 +0700

    cms website

--- cms_news/src/pages/Home.jsx
+++ cms_news/src/pages/Home.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react"
+
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [articles, setArticles] = useState(null)
+    
+    useEffect(() => {
+        const fetchArticle = async () => {
+            const response = await fetch('/api/articles')
+            const article = await response.json()
+
+            if(response.ok){
+                setArticles(article)
+            }
+        }
+
+        fetchArticle()
+    }, [])
+
+    return (
+    <div>
+        <h1>NEWS ARTICLE</h1>
+        <div>
+            { articles && articles.map((article) => (
+                <div key={article._id}>
+                    <h1>{article.title}</h1>
+                </div>
+            ))
+
+            }
+        </div>
+    </div>
+)
 }
 
 export default Home
